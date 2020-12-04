483a409e7486ef87b8544b8b5d73add1ecd8c536
amelbougrine <amel.1989ab@gmail.com>
2020-12-04 23:57:46 +0100

    2nd commit

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,10 +1,53 @@
-﻿/**
+﻿// =RecSheets()  --> Feuilles
+// =RecRows()    --> Lignes
+// =RecColumns() --> Colonnes
+// =RecSum(Table, Champ, Filtre) --> Somme
+// =RecCount(Table, Filtre1, Filtre2) --> Compter le nombre d'enregistrement
+/**
  * Sum of a single range
  * @customfunction
- * @param {number[][]} singleRange a single range
+ * @param {number[][]} singleRange  a single range
+ */
+function Sheets() {
+  // selectedSheet.getUsedRange().getFormat().autofitColumns();
+  let selectedSheet = context.workbook.worksheets.getItem("Sheet1");
+  let worksheet = worksheet
+  let singleRange = header;
+  let total = 0;
+  singleRange.forEach(setOfSingleValues => {
+    setOfSingleValues.forEach(value => {
+      total += value;
+    })
+  })
+  return total;
+}/**
+* The sum of all of the numbers.
+* @customfunction
+* @param {number[][][]} operands A number (such as 1 or 3.1415), a cell address (such as A1 or $E$11), or a range of cell addresses (such as B3:F12)
+* @returns {number} The sum of all of the numbers.
+*/
+function Sum(operands) {
+  let total = 0;
+
+  operands.forEach(range => {
+    range.forEach(row => {
+      row.forEach(num => {
+        total += num;
+      });
+    });
+  });
+
+  return total;
+}
+/**
+ * Sum of a single range
+ * @customfunction
+ * @param {number[][]} singleRange  a single range
  */
 function Sum(header) {
-  let singleRange = header
+  // selectedSheet.getUsedRange().getFormat().autofitColumns();
+  let worksheet = worksheet
+  let singleRange = header;
   let total = 0;
   singleRange.forEach(setOfSingleValues => {
     setOfSingleValues.forEach(value => {
