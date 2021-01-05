68b90ce41dc2d58d9abbb4ae5be24b1cfe09cb6a
amelbougrine <amel.1989ab@gmail.com>
2021-01-05 16:27:00 +0100

    Dynamic Display

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -4,41 +4,54 @@
 // =RecSum(Table, Champ, Filtre) --> Somme
 // =RecCount(Table, Filtre1, Filtre2) --> Compter le nombre d'enregistrement
 //=RecDistinctRows()
+var data1;
+async function call() {
+  await ShowData();
+}
+call();
 /**
  * Get text values that spill down.
  * @customfunction
  * @returns {string[][]} A dynamic array with multiple results.
  */
 function spillDown() {
-  return [['first'], ['second'], ['third']];
+  return [[data1.Date], [data1.Montant], ['third']];
 }
 /**
-  * Gets the star count for a given Github repository.
-  * @customfunction 
-  * @return {string} Data
-  */
+ * Get text values that spill to the right.
+ * @customfunction
+ * @returns {string[][]} A dynamic array with multiple results.
+ */
+function spillRight() {
+  return [[data1.Date, data1.Montant, 'third']];
+}
+// /**
+//   * Gets the star count for a given Github repository.
+//   * @customfunction 
+//   * @return {string} Data
+//   */
  function ShowData() {
   var ourRequest = new XMLHttpRequest();
   ourRequest.open('GET', 'https://raw.githubusercontent.com/amelbougrine/Office-test/main/bank.json');
   ourRequest.onload = function() {
     if (ourRequest.status >= 200 && ourRequest.status < 400) {
       var ourData = JSON.parse(ourRequest.responseText);
-      let data = JSON.stringify(ourData);
-      return data[0];
+      data1 = ourData[0];
+      return;
     } else {
       let data = "We connected to the server, but it returned an error: " + ourRequest.status;
-      return data;
+      data1 = data;
+      return;
     }
-    
   };
-
   ourRequest.onerror = function() {
     let data = "Connection error";
-    return data;
+    data1 = data;
+    return;
   };
-
   ourRequest.send();
 }
+
 /**
  * Select Worksheets
  * @customfunction
